47565bf807078fed79df58f0a0b40c4d211a71ac
raj <nagaraja.kambathalli@kaplan.edu>
2024-03-09 09:41:05 +0530

    quick access your tabs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const data = [
+  {
+    name: "Default checkins",
+    url: [
+      "https://www.google.com/",
+      "https://mail.google.com/mail/u/0",
+      "https://kna-core.atlassian.net/jira/software/c/projects/TEACH/boards/1094?quickFilter=8407&quickFilter=7008",
+    ],
+  },
+];
 
 function App() {
+  const openLinks = (url) => {
+    for (const link of url) {
+      window.open(link, "_blank");
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, Quick Access for your tabs...</h1>
+      <div className="lists">
+        {data.map((item) => {
+          return (
+            <button className="button" onClick={() => openLinks(item.url)}>
+              {item.name}
+            </button>
+          );
+        })}
+      </div>
     </div>
   );
 }
