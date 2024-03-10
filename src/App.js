7a4ac944baae8ee2f029da1f8a5e1b2c55fd5483
raj <nagaraja.kambathalli@kaplan.edu>
2024-03-10 23:03:43 +0530

    url changes

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ const data = [
   {
     name: "Default checkins",
     url: [
-      "https://www.google.com/",
       "https://mail.google.com/mail/u/0",
+      "https://calendar.google.com/calendar/u/0",
       "https://kna-core.atlassian.net/jira/software/c/projects/TEACH/boards/1094?quickFilter=8407&quickFilter=7008",
+      "https://github.com/KaplanTestPrep/atom-web/pulls/nagarajkaplan",
+      "https://kna.qa.learnwithatom.com/",
+      "http://localhost:5100/educator/reporting/organizations/a19df7a0-9349-4234-8965-4ae9a8183a3c",
     ],
   },
 ];
